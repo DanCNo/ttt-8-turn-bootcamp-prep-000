1380d4fd0cb0c7211177d12ab74f0627823fe0ca
Dan N <dan.noh@gmail.com>
2018-08-29 19:06:16 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,9 @@
 #display_board
 
-def display_board(board_array)
-  puts " #{board_array[0]} | #{board_array[1]} | #{board_array[2]} \n-----------\n #{board_array[3]} | #{board_array[4]} | #{board_array[5]} \n-----------\n #{board_array[6]} | #{board_array[7]} | #{board_array[8]} \n"
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} \n"
 end
 
+#valid_move?
+
+def valid_move?(board, user_index)
