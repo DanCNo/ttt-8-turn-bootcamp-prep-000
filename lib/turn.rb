46ac996538ac0be145b2726eaa510f2f883d7a58
Dan N <dan.noh@gmail.com>
2018-08-29 20:02:18 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -52,12 +52,12 @@ def turn(board)
   
   if valid_move?(board, index)
     move(board, index, "X")
+    display_board(board)
 
   else turn(board)
   
   end
   
-  #display_board(board)
   
   
 end
